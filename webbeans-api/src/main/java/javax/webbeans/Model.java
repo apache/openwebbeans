70ec3ec0e66fe15090674de008a63d5364e991a2
Gurkan Erdogdu <gerdogdu@apache.org>
2008-12-27 22:39:13 +0000

    ElementType.FIELD is added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@729692 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-api/src/main/java/javax/webbeans/Model.java
+++ webbeans-api/src/main/java/javax/webbeans/Model.java
@@ -18,6 +18,7 @@ package javax.webbeans;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
@@ -28,7 +29,7 @@ import java.lang.annotation.Target;
 @RequestScoped
 @Production
 @Stereotype
-@Target( { TYPE, METHOD })
+@Target( { TYPE, METHOD, FIELD })
 @Retention(RUNTIME)
 @Documented
 public @interface Model
