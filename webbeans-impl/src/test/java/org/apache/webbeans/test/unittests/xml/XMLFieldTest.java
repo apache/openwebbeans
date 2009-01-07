f86a509e034dabd7b426915533c435b1339b09af
Gurkan Erdogdu <gerdogdu@apache.org>
2009-01-07 20:03:57 +0000

    Test case update from Mark Struberg. Thanks Mark!
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@732454 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/test/unittests/xml/XMLFieldTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/test/unittests/xml/XMLFieldTest.java
@@ -25,6 +25,7 @@ import junit.framework.Assert;
 
 import org.apache.webbeans.container.ManagerImpl;
 import org.apache.webbeans.test.servlet.TestContext;
+import org.apache.webbeans.test.xml.ComponentForField;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -65,7 +66,32 @@ public class XMLFieldTest extends TestContext
 			InputStream stream = XMLFieldTest.class.getClassLoader().getResourceAsStream("org/apache/webbeans/test/xml/fieldTest.xml");			
 			Assert.assertNotNull(stream);
 			
+			/*Clear the manager component list*/
+			clear();
+						
 			this.xmlConfigurator.configure(stream,"fieldTest.xml");
+			
+			ComponentForField cff = (ComponentForField) container.getInstanceByName("componentForField");
+			
+			Assert.assertNotNull(cff);
+			Assert.assertEquals(35, cff.getIntField());
+			Assert.assertEquals(35.3f, cff.getFloatField());
+			Assert.assertEquals(35.5d, cff.getDoubleField());
+			Assert.assertEquals('a', cff.getCharField());
+			Assert.assertEquals(37, cff.getLongField());
+			Assert.assertEquals(1, cff.getByteField());
+			Assert.assertEquals(5, cff.getShortField());
+			Assert.assertEquals(true, cff.isBooleanField());
+			Assert.assertEquals("ENUM1", cff.getEnum1().name());
+			Assert.assertEquals("dskfj", cff.getStrField());
+			Assert.assertNotNull(cff.getDateField());
+			Assert.assertNotNull(cff.getCalendarField());
+			Assert.assertEquals(ComponentForField.class, cff.getClazzField());
+			Assert.assertNotNull(cff.getListStrField());
+			Assert.assertNotNull(cff.getListEnumField());
+			
+			
+			
 			
 		}catch(Throwable e1)
 		{
