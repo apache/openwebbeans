ff2c235e310581c9d9b9865a01c658e1bcc75f41
Gurkan Erdogdu <gerdogdu@apache.org>
2009-01-17 08:09:06 +0000

    Static producer component and broken tests are added.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@735244 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-impl/src/test/java/org/apache/webbeans/test/unittests/producer/StaticProducerTest.java
@@ -0,0 +1,67 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one or more
+ *  contributor license agreements.  See the NOTICE file distributed with
+ *  this work for additional information regarding copyright ownership.
+ *  The ASF licenses this file to You under the Apache License, Version 2.0
+ *  (the "License"); you may not use this file except in compliance with
+ *  the License.  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.apache.webbeans.test.unittests.producer;
+
+import java.lang.annotation.Annotation;
+
+import org.apache.webbeans.component.ProducerComponentImpl;
+import org.apache.webbeans.context.ContextFactory;
+import org.apache.webbeans.test.component.producer.StaticProducer1;
+import org.apache.webbeans.test.servlet.TestContext;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class StaticProducerTest extends TestContext
+{
+    
+    public StaticProducerTest()
+    {
+        super(StaticProducerTest.class.getName());
+    }
+
+    @Before
+    public void init()
+    {
+        initDefaultDeploymentTypes();
+    }
+    
+    @Test
+    public void testStaticProducer1()
+    {
+        clear();
+        
+        ContextFactory.initRequestContext(null);
+        
+        defineSimpleWebBean(StaticProducer1.class);
+        
+        ProducerComponentImpl<?> pc = (ProducerComponentImpl<?>) getManager().resolveByName("weight").iterator().next();
+        
+        Object obj = getManager().getInstance(pc);
+        
+       Assert.assertTrue(obj instanceof Integer);
+       Assert.assertEquals(79, obj);
+       
+       pc = (ProducerComponentImpl<?>)getManager().resolveByType(int.class, new Annotation[]{}).iterator().next();
+       
+       obj = getManager().getInstance(pc);
+       
+       Assert.assertTrue(obj instanceof Integer);
+       Assert.assertEquals(79, obj);       
+       
+    }
+}
