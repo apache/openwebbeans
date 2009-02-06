687e15be03076dba1cc34680b72b382569cafa15
Mark Struberg <struberg@apache.org>
2009-02-06 12:16:58 +0000

    OWB-75 create integration tests for webbeans-impl
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@741551 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-impl/src/it/properties/src/test/java/org/apache/webbeans/test/spi/configuration/ConfigurationTest.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.apache.webbeans.test.spi.configuration;
+
+import junit.framework.Assert;
+
+import org.apache.webbeans.config.OpenWebBeansConfiguration;
+import org.junit.Test;
+
+public class ConfigurationTest
+{
+    @Test
+    public void testConfigurationParser() 
+    {
+        OpenWebBeansConfiguration cfg = OpenWebBeansConfiguration.getInstance();
+        
+        // not overloaded
+        String ts = cfg.getProperty("org.apache.webbeans.spi.TransactionService");
+        Assert.assertNotNull(ts);
+        Assert.assertEquals("org.apache.webbeans.spi.ee.TransactionServiceJndiImpl", ts);
+        
+        // overloaded version 1
+        String wbf = cfg.getProperty("org.apache.webbeans.spi.JNDIService");
+        Assert.assertNotNull(wbf);
+        Assert.assertEquals("org.apache.webbeans.spi.ee.JNDIServiceEnterpriseImpl", wbf);
+        
+        // property which is only in the specialised openwebbeans.properties
+        String testProperty = cfg.getProperty("test.property");
+        Assert.assertNotNull(testProperty);
+        Assert.assertEquals("true", testProperty);
+    }
+    
+}
