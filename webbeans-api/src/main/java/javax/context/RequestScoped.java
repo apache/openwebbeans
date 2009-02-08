f5269e418239f077aad719ca345ec837c825ac2a
Gurkan Erdogdu <gerdogdu@apache.org>
2009-02-08 10:55:12 +0000

    Adding type field.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@742047 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-api/src/main/java/javax/context/RequestScoped.java
+++ webbeans-api/src/main/java/javax/context/RequestScoped.java
@@ -15,13 +15,14 @@ package javax.context;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target( { TYPE, METHOD })
+@Target( { TYPE, METHOD, FIELD })
 @Retention(RUNTIME)
 @Documented
 @ScopeType
--- webbeans-api/src/main/java/javax/inject/Produces.java
+++ webbeans-api/src/main/java/javax/inject/Produces.java
@@ -14,13 +14,14 @@
 package javax.inject;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target(METHOD)
+@Target({METHOD,FIELD})
 @Retention(RUNTIME)
 @Documented
 public @interface Produces
