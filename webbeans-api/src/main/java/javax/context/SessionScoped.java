229736b3a722d2e4b3e4876c00520b5bb3e7a301
Gurkan Erdogdu <gerdogdu@apache.org>
2009-02-21 16:36:55 +0000

    Add TYPE support.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@746537 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-api/src/main/java/javax/context/SessionScoped.java
+++ webbeans-api/src/main/java/javax/context/SessionScoped.java
@@ -14,6 +14,7 @@
 package javax.context;
 
 import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -21,7 +22,7 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-@Target( { TYPE, METHOD })
+@Target( { TYPE, METHOD, FIELD})
 @Retention(RUNTIME)
 @Documented
 @ScopeType
