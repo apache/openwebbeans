729b865fac5488b416a81d96306ec8fa1d35aee9
Gurkan Erdogdu <gerdogdu@apache.org>
2009-02-27 19:26:45 +0000

    Changing API package for Model class to run the TCK succesfully.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@748647 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-api/src/main/java/javax/webbeans/Model.java
+++ webbeans-api/src/main/java/javax/webbeans/Model.java
@@ -11,7 +11,7 @@
  * KIND, either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package javax.annotation;
+package javax.webbeans;
 
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
@@ -22,6 +22,8 @@ import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+import javax.annotation.Named;
+import javax.annotation.Stereotype;
 import javax.context.RequestScoped;
 import javax.inject.Production;
 
