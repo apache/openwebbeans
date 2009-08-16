07bcf643d7587d719eaa3989d646a3ed1623d05f
Gurkan Erdogdu <gerdogdu@apache.org>
2009-08-16 20:06:19 +0000

    Adding EJB sample project
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@804756 13f79535-47bb-0310-9956-ffa450edef68

---
+++ samples/ejb-sample/src/main/java/org/apache/webbeans/sample/bean/EchoManaged.java
@@ -0,0 +1,61 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.apache.webbeans.sample.bean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.Current;
+import javax.enterprise.inject.Named;
+
+import org.apache.webbeans.sample.ejb.Echo;
+
+
+@RequestScoped
+@Named("echo")
+public class EchoManaged
+{
+    private @Current Echo echo;
+
+    private String text;
+    
+    private String name;
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String echo()
+    {
+        this.text = echo.echo(name);
+        
+        return null;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+    
+    
+}
