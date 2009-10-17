5ed9ae8dd81403250c783cea6c742fca31f46cd9
Gurkan Erdogdu <gerdogdu@apache.org>
2009-10-17 20:42:11 +0000

    Adding singleton context.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@826306 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-impl/src/main/java/org/apache/webbeans/portable/creation/DefaultInjectionTargetImpl.java
@@ -0,0 +1,70 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.apache.webbeans.portable.creation;
+
+import java.util.Set;
+
+import javax.enterprise.context.spi.CreationalContext;
+import javax.enterprise.inject.spi.AnnotatedType;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.enterprise.inject.spi.InjectionTarget;
+
+import org.apache.webbeans.config.ManagedBeanConfigurator;
+
+public class DefaultInjectionTargetImpl<T> implements InjectionTarget<T>
+{
+    private InjectionTargetProducer<T> target;
+    
+    private DefaultInjectionTargetImpl(AnnotatedType<T> annotatedType)
+    {
+        target = new InjectionTargetProducer<T>(ManagedBeanConfigurator.defineFromAnnotatedType(annotatedType));
+    }
+
+    @Override
+    public void inject(T instance, CreationalContext<T> ctx)
+    {
+        this.target.inject(instance, ctx);
+    }
+
+    @Override
+    public void postConstruct(T instance)
+    {
+        this.target.postConstruct(instance);
+    }
+
+    @Override
+    public void preDestroy(T instance)
+    {
+        this.target.preDestroy(instance);
+    }
+
+    @Override
+    public void dispose(T instance)
+    {
+        this.target.dispose(instance);
+    }
+
+    @Override
+    public Set<InjectionPoint> getInjectionPoints()
+    {
+        return this.target.getInjectionPoints();
+    }
+
+    @Override
+    public T produce(CreationalContext<T> creationalContext)
+    {
+        return this.target.produce(creationalContext);
+    }
+
+}
