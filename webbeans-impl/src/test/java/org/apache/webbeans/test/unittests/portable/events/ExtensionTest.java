ff3aed336048fd6f5190af38c54cfce1d1ae45f9
Mark Struberg <struberg@apache.org>
2009-11-18 22:20:05 +0000

    OWB-148 move the Extension test to the new testing framework
    This way we can test if the 'real' events got fired!
    
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/openwebbeans/trunk@881965 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/test/unittests/portable/events/ExtensionTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/test/unittests/portable/events/ExtensionTest.java
@@ -19,62 +19,64 @@
 package org.apache.webbeans.test.unittests.portable.events;
 
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import javax.enterprise.inject.spi.Bean;
 
 import junit.framework.Assert;
 
 import org.apache.webbeans.annotation.DefaultLiteral;
-import org.apache.webbeans.common.TestContext;
+import org.apache.webbeans.common.AbstractUnitTest;
 import org.apache.webbeans.context.ContextFactory;
-import org.apache.webbeans.portable.events.ExtensionLoader;
 import org.apache.webbeans.portable.events.discovery.BeforeShutdownImpl;
 import org.apache.webbeans.test.component.library.BookShop;
 import org.apache.webbeans.test.component.portable.events.MyExtension;
+import org.apache.webbeans.test.component.producer.primitive.PrimitiveProducer;
 import org.apache.webbeans.test.mock.MockServletContext;
-import org.junit.Before;
 import org.junit.Test;
 
-public class ExtensionTest extends TestContext
+/**
+ * This test checks if an extension gets loaded correctly and
+ * if all specified events get fired.
+ */
+public class ExtensionTest extends AbstractUnitTest
 {
     public ExtensionTest()
     {
-        super(ExtensionTest.class.getName());
     }
 
-    @Before
-    public void init()
-    {
-        super.init();
-    }
-    
     @Test
     @SuppressWarnings("unchecked")
     public void testExtensionServices()
     {
         MyExtension.reset();
-        
-        ExtensionLoader.getInstance().loadExtensionServices();
-        
+
+        Collection<Class<?>> classes = new ArrayList<Class<?>>();
+        classes.add(BookShop.class);
+        classes.add(PrimitiveProducer.class);
+        startContainer(classes);
+
         MockServletContext servletContext = new MockServletContext();
         ContextFactory.initApplicationContext(servletContext);
 
-        Bean<MyExtension> extension = (Bean<MyExtension>)getManager().resolveByType(MyExtension.class, new DefaultLiteral()).iterator().next();
+        Bean<MyExtension> extension = (Bean<MyExtension>) getBeanManager().getBeans(MyExtension.class, new DefaultLiteral()).iterator().next();
         
-        MyExtension ext = getManager().getInstance(extension);
+        MyExtension ext = (MyExtension) getBeanManager().getReference(extension, MyExtension.class, getBeanManager().createCreationalContext(extension));
         
         System.out.println(ext.toString());
         
-        defineManagedBean(BookShop.class);
-        
         Assert.assertNotNull(MyExtension.processAnnotatedTypeEvent);
-
-        //X TODO should work after fixing the test lifecycle Assert.assertNotNull(MyExtension.processBean);
-
-        //X TODO should work after fixing the test lifecycle Assert.assertNotNull(MyExtension.processObserverMethod);
+        Assert.assertNotNull(MyExtension.processBean);
+        Assert.assertNotNull(MyExtension.processObserverMethod);
+        Assert.assertNotNull(MyExtension.lastAfterBeanDiscovery);
+        Assert.assertNotNull(MyExtension.lastBeforeBeanDiscovery);
+        Assert.assertNotNull(MyExtension.afterDeploymentValidation);
+        Assert.assertNotNull(MyExtension.processInjectionTarget);
+        Assert.assertNotNull(MyExtension.processProducer);
         
         //Fire shut down
-        getManager().fireEvent(new BeforeShutdownImpl(), new Annotation[0]);
+        getBeanManager().fireEvent(new BeforeShutdownImpl(), new Annotation[0]);
 
         ContextFactory.destroyApplicationContext(servletContext);
         
