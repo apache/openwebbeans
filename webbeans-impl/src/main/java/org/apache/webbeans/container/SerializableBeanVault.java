940eacfa1540427702bb1e3ae928c21b6999db14
Mark Struberg <struberg@apache.org>
2010-02-05 19:54:11 +0000

    OWB-6 fixed SerializableBeanVault and improved JavaDoc
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@907073 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/main/java/org/apache/webbeans/container/SerializableBeanVault.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/container/SerializableBeanVault.java
@@ -24,11 +24,23 @@ import org.apache.webbeans.config.WebBeansFinder;
 import javax.enterprise.context.spi.Contextual;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.PassivationCapable;
+import java.io.Serializable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * This class acts as a storage for {@link SerializableBean}s
+ * <p>This class acts as a storage for {@link SerializableBean}s.</p>
+ *
+ * <h3>The Background:</h3>
+ * <p>Any Contextual&lt;T&gt; which holds his information on a storage which may get serialized,
+ * like e.g. the SessionContext, the ConversationContext or any 3-rd party Context
+ * for a NormalScoped(passivating=true) and therefore PassivationCapable Scope needs to be Serializable.</p>
+ * <p>Normal {@link Bean}s are not serializable because they contain non transportable information. But each
+ * {@link PassivationCapable} {@link Bean} is uniquely identifyable via it's id and can be restored by
+ * {@link javax.enterprise.inject.spi.BeanManager#getPassivationCapableBean(String)}.</p>
+ * <p>Since a custom Context implementation doesn't know when it gets passivated (for a Http Session
+ * this is pretty seldom), the Contextual<T> handed over to the Context implementation must be
+ * Serializable.</p>
  */
 public class SerializableBeanVault {
 
@@ -41,21 +53,22 @@ public class SerializableBeanVault {
 
     public <T> Contextual<T> getSerializableBean(Contextual<T> bean)
     {
-        if (bean instanceof SerializableBean)
+        if (bean instanceof Serializable)
         {
+            // we don't need to wrap beans which are already Serializable
             return bean;
         }
 
-        if (bean instanceof SerializableBean && bean instanceof AbstractBean)
+        if (bean instanceof AbstractBean)
         {
-            AbstractBean ab = (AbstractBean)bean;
+            AbstractBean<T> ab = (AbstractBean<T>)bean;
             if (ab.isSerializable())
             {
                 String id = ab.getId();
-                SerializableBean sb = serializableBeans.get(id);
+                SerializableBean<T> sb = (SerializableBean<T>) serializableBeans.get(id);
                 if (sb == null)
                 {
-                    sb = new SerializableBean((Bean<?>) bean);
+                    sb = new SerializableBean<T>((Bean<T>) bean);
                     serializableBeans.put(id, sb);
                 }
 
@@ -63,6 +76,7 @@ public class SerializableBeanVault {
             }
         }
 
+        // this should not happen, because all our beans are AbstractBeans!
         return null;
     }
 }
