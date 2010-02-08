df743ba1cf61b54fa1f9915d69aa9e5aa4456bb7
Mark Struberg <struberg@apache.org>
2010-02-08 11:04:53 +0000

    OWB-270 remove EJB dependency from openwebbeans-resource
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@907619 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-resource/src/main/java/org/apache/webbeans/resource/OpenWebBeansResourcePlugin.java
+++ webbeans-resource/src/main/java/org/apache/webbeans/resource/OpenWebBeansResourcePlugin.java
@@ -18,7 +18,6 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Type;
 
 import javax.annotation.Resource;
-import javax.ejb.EJB;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
@@ -40,8 +39,7 @@ public class OpenWebBeansResourcePlugin extends AbstractOpenWebBeansPlugin imple
         if (annotationClass.equals(Resource.class) || 
                 annotationClass.equals(WebServiceRef.class) || 
                 annotationClass.equals(PersistenceUnit.class) || 
-                annotationClass.equals(PersistenceContext.class) ||
-                annotationClass.equals(EJB.class))
+                annotationClass.equals(PersistenceContext.class))
         {
             return true;
         }
