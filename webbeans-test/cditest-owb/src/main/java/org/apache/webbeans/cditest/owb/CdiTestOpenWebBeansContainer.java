196ca5b1992f74ba0dda26305e7f3b9d1882b54a
Mark Struberg <struberg@apache.org>
2010-03-05 19:35:09 +0000

    OWB-316 implement generic CDI test container + OWB provider integration
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@919555 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-test/cditest-owb/src/main/java/org/apache/webbeans/cditest/owb/CdiTestOpenWebBeansContainer.java
@@ -0,0 +1,179 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.webbeans.cditest.owb;
+
+import java.lang.annotation.Annotation;
+import java.util.Set;
+
+import javax.enterprise.inject.AmbiguousResolutionException;
+import javax.enterprise.inject.ResolutionException;
+import javax.enterprise.inject.spi.Bean;
+import javax.enterprise.inject.spi.BeanManager;
+
+import org.apache.webbeans.cditest.CdiTestContainer;
+import org.apache.webbeans.context.ContextFactory;
+import org.apache.webbeans.lifecycle.LifecycleFactory;
+import org.apache.webbeans.spi.ContainerLifecycle;
+
+/**
+ * OpenWebBeans specific implementation of {@link CdiTestContainer}.
+ */
+public class CdiTestOpenWebBeansContainer implements CdiTestContainer 
+{
+
+    private ContainerLifecycle  lifecycle = null;
+    private MockServletContext  servletContext = null;
+    private MockHttpSession     session = null;
+
+    @Override
+    public void bootContainer() throws Exception 
+    {
+        servletContext = new MockServletContext();
+        session = new MockHttpSession();
+        lifecycle = LifecycleFactory.getInstance().getLifecycle();
+        lifecycle.start(servletContext);
+    }
+
+    @Override
+    public void shutdownContainer() throws Exception 
+    {
+        if (lifecycle != null) 
+        {
+            lifecycle.stop(servletContext);
+        }
+    }
+
+    @Override
+    public void startContexts() throws Exception 
+    {
+        ContextFactory.initSingletonContext(servletContext);
+        ContextFactory.initApplicationContext(servletContext);
+        ContextFactory.initSessionContext(session);
+        ContextFactory.initConversationContext(null);
+        ContextFactory.initRequestContext(null);
+    }
+
+    @Override
+    public void startApplicationScope() throws Exception 
+    {
+        ContextFactory.initApplicationContext(servletContext);
+    }
+
+    @Override
+    public void startConversationScope() throws Exception 
+    {
+        ContextFactory.initConversationContext(null);
+    }
+
+    @Override
+    public void startCustomScope(Class<? extends Annotation> scopeClass) throws Exception 
+    {
+        //X TODO
+    }
+
+    @Override
+    public void startRequestScope() throws Exception 
+    {
+        ContextFactory.initRequestContext(null);
+    }
+
+    @Override
+    public void startSessionScope() throws Exception 
+    {
+        ContextFactory.initSessionContext(session);
+    }
+
+    @Override
+    public void stopContexts() throws Exception 
+    {
+        ContextFactory.destroyRequestContext(null);
+        ContextFactory.destroyConversationContext();
+        ContextFactory.destroySessionContext(session);
+        ContextFactory.destroyApplicationContext(servletContext);
+        ContextFactory.destroySingletonContext(servletContext);
+    }
+
+    @Override
+    public void stopApplicationScope() throws Exception 
+    {
+        ContextFactory.destroyApplicationContext(servletContext);
+    }
+
+    @Override
+    public void stopConversationScope() throws Exception 
+    {
+        ContextFactory.destroyConversationContext();
+    }
+
+    @Override
+    public void stopCustomScope(Class<? extends Annotation> scopeClass) throws Exception 
+    {
+        //X TODO
+    }
+
+    @Override
+    public void stopRequestScope() throws Exception 
+    {
+        ContextFactory.destroyRequestContext(null);
+    }
+
+    @Override
+    public void stopSessionScope() throws Exception 
+    {
+        ContextFactory.destroySessionContext(session);
+    }
+    
+    public  BeanManager getBeanManager() 
+    {
+        return lifecycle.getBeanManager();
+    }
+
+    @Override
+    public <T> T getInstance(Class<T> type, Annotation... qualifiers)
+    throws ResolutionException 
+    {
+        Set<Bean<?>> beans = getBeanManager().getBeans(type, qualifiers);
+        if (beans == null || beans.isEmpty()) 
+        {
+            throw new ResolutionException("cannot find beans for class " + type.getCanonicalName());
+        }
+
+        if (beans.size() > 1) 
+        {
+            throw new AmbiguousResolutionException("cannot find beans for class " + type.getCanonicalName());
+        }
+
+        @SuppressWarnings("unchecked")
+        Bean<T> bean = (Bean<T>)beans.iterator().next();
+
+        @SuppressWarnings("unchecked")
+        T instance = (T) getBeanManager().getReference(bean, type, getBeanManager().createCreationalContext(bean));
+        return instance;
+    }
+
+    @Override
+    public Object getInstance(String name)
+    throws ResolutionException 
+    {
+        //X getBeanManager().getELResolver();
+        // TODO implement
+        return null;
+    }
+
+}
