a096906912069b333ca8ed4a18fb1e85f58933e4
Mark Struberg <struberg@apache.org>
2010-04-12 11:44:22 +0000

    OWB-351 this simple test shows the problems with our CreationalContexts
    The test takes an @ApplicationScoped bean which got a proxy for a 
    @SessionScoped bean injected, and verifies that we get different @SessionScoped
    contextual instances for different sessions.
    
    Attention: this currently breaks the build, but this is a real blocker!
    
    
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@933198 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/newtests/contexts/session/tests/SessionContextTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/newtests/contexts/session/tests/SessionContextTest.java
@@ -1,26 +1,33 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to You under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
- * or agreed to in writing, software distributed under the License is
- * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the specific language
- * governing permissions and limitations under the License.
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 package org.apache.webbeans.newtests.contexts.session.tests;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
+import javax.enterprise.context.SessionScoped;
 import javax.enterprise.inject.spi.Bean;
 
 import junit.framework.Assert;
 
 import org.apache.webbeans.newtests.AbstractUnitTest;
+import org.apache.webbeans.newtests.contexts.session.common.AppScopedBean;
 import org.apache.webbeans.newtests.contexts.session.common.PersonalDataBean;
 import org.junit.Test;
 
@@ -54,4 +61,28 @@ public class SessionContextTest extends AbstractUnitTest
         Assert.assertTrue(PersonalDataBean.PRE_DESTROY);
     }
 
+    @Test
+    public void testInstanceCreation()
+    {
+        Collection<Class<?>> classes = new ArrayList<Class<?>>();
+        classes.add(PersonalDataBean.class);
+        classes.add(AppScopedBean.class);
+        
+        startContainer(classes);
+        
+        AppScopedBean appBeanInstance = getInstance(AppScopedBean.class);
+        Assert.assertNotNull(appBeanInstance);
+        PersonalDataBean pdb1 = appBeanInstance.getPdb().getInstance();
+        Assert.assertNotNull(pdb1);
+        
+        // now we reset the session Context so we should get a new contextual instance.
+        getLifecycle().getContextService().endContext(SessionScoped.class, null);
+        getLifecycle().getContextService().startContext(SessionScoped.class, null);
+        
+        PersonalDataBean pdb2 = appBeanInstance.getPdb().getInstance();
+        Assert.assertNotNull(pdb2);
+        
+        // pdb1 and pdb2 are in different sessions, so they must not be the same instance!
+        Assert.assertTrue(pdb1 != pdb2);
+    }
 }
