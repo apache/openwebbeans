ec8cf2700c448f44cebd71edc8d33dc9299d436d
Gurkan Erdogdu <gerdogdu@apache.org>
2010-04-22 07:43:22 +0000

    lackof license
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@936636 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/main/java/org/apache/webbeans/util/SecurityUtil.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/util/SecurityUtil.java
@@ -1,11 +1,22 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with this
+ * work for additional information regarding copyright ownership. The ASF
+ * licenses this file to You under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0 Unless required by applicable law
+ * or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
 package org.apache.webbeans.util;
 
-import java.util.Set;
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.security.AccessControlContext;
 import java.security.AccessController;
 import java.security.PrivilegedAction;
 import javassist.util.proxy.ProxyFactory;
@@ -28,8 +39,6 @@ public class SecurityUtil
 		@SuppressWarnings("unchecked")
 		public static <T> Constructor<T> doPrivilegedGetDeclaredConstructor(Class<T> clazz, Class<?>... parameterTypes) throws NoSuchMethodException
 		{
-			ClassLoader ld = SecurityUtil.class.getClassLoader();
-			AccessControlContext acc = AccessController.getContext();
 			Object obj = AccessController.doPrivileged(
 					new PrivilegedActionForClass(clazz, parameterTypes, METHOD_CLASS_GETDECLAREDCONSTRUCTOR));
 			if (obj instanceof NoSuchMethodException) throw (NoSuchMethodException)obj;
