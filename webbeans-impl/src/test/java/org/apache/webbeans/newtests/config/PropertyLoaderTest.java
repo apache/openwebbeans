ed4ed9eca62c59507af8c1de020db930e32be38b
Gurkan Erdogdu <gerdogdu@apache.org>
2010-08-17 15:30:39 +0000

    more test method for PropertyLoaderTest
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@986359 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/newtests/config/PropertyLoaderTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/newtests/config/PropertyLoaderTest.java
@@ -22,11 +22,15 @@ import org.apache.webbeans.config.PropertyLoader;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 public class PropertyLoaderTest
 {
     private static final String PROPERTY_FILE = "org/apache/webbeans/newtests/config/propertyloadertest.properties";
+    private static final String PROPERTY_FILE2 = "org/apache/webbeans/newtests/config/propertyloadertest2.properties";
+    private static final String PROPERTY_FILE3 = "org/apache/webbeans/newtests/config/propertyloadertest3.properties";
 
     @Test
     public void testPropertyLoader() throws Exception
@@ -44,5 +48,30 @@ public class PropertyLoaderTest
     {
         Properties p = PropertyLoader.getProperties("notexisting.properties");
         Assert.assertNull(p);
+    }
+    
+    @Test
+    public void testOrdinal()
+    {
+        Properties p15 = PropertyLoader.getProperties(PROPERTY_FILE);
+        Properties p16 = PropertyLoader.getProperties(PROPERTY_FILE2);
+        Properties p20 = PropertyLoader.getProperties(PROPERTY_FILE3);
+        
+        List<Properties> properties = new ArrayList<Properties>();
+        properties.add(p15);
+        properties.add(p16);
+        properties.add(p20);
+        
+        Properties prop = MockPropertyLoader.mergeProperties(MockPropertyLoader.sortProperties(properties));
+        Assert.assertEquals("testValue16", prop.get("testConfig"));
+        Assert.assertEquals("16", prop.get("test16"));
+        Assert.assertEquals("15", prop.get("test15"));
+        Assert.assertEquals("20", prop.get("configuration.ordinal"));
+        Assert.assertEquals("z", prop.get("override_y"));
+        Assert.assertEquals("20", prop.get("override_all"));
+        Assert.assertEquals("15", prop.get("unique_1"));
+        Assert.assertEquals("16", prop.get("unique_2"));
+        Assert.assertEquals("20", prop.get("unique_3"));
+        
     }
 }
