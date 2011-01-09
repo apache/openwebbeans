48319c1d0c67f97e3a9405af6b2791d81e9aa35d
Gurkan Erdogdu <gerdogdu@apache.org>
2011-01-09 11:26:13 +0000

    [OWB-514], Recommit overrided changes
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1056915 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-jsf12/src/main/java/org/apache/webbeans/jsf12/plugin/OpenWebBeansJsfPlugin.java
+++ webbeans-jsf12/src/main/java/org/apache/webbeans/jsf12/plugin/OpenWebBeansJsfPlugin.java
@@ -18,15 +18,19 @@
  */
 package org.apache.webbeans.jsf12.plugin;
 
-import javax.faces.component.UIComponent;
 
+import javax.faces.component.UIComponent;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
+
+import org.apache.webbeans.config.OpenWebBeansConfiguration;
 import org.apache.webbeans.exception.WebBeansConfigurationException;
-import org.apache.webbeans.spi.plugins.AbstractOwbPlugin;
+import org.apache.webbeans.spi.plugins.AbstractOwbJsfPlugin;
 import org.apache.webbeans.util.ClassUtil;
 
-public class OpenWebBeansJsfPlugin extends AbstractOwbPlugin
+public class OpenWebBeansJsfPlugin extends AbstractOwbJsfPlugin
 {
-
     /** {@inheritDoc} */
     public void isManagedBean( Class<?> clazz ) throws WebBeansConfigurationException 
     {
@@ -36,4 +40,23 @@ public class OpenWebBeansJsfPlugin extends AbstractOwbPlugin
                                                      + " can not implement JSF UIComponent");
         }
     }
+
+    @Override
+    public boolean isOwbApplication()
+    {
+        FacesContext facesContext = FacesContext.getCurrentInstance();
+        if(facesContext == null)
+        {
+            throw new IllegalStateException("FacesContext is null");
+        }
+        
+        ExternalContext ext = facesContext.getExternalContext();
+        ServletContext servletContext = (ServletContext) ext.getContext();
+        Object attribute = servletContext.getAttribute(OpenWebBeansConfiguration.PROPERTY_OWB_APPLICATION);
+        
+        return attribute != null ? true : false;
+    }
+    
+    
+    
 }
