6ea7ecf8acb0a5b2e8931eda309595b5b4f1bb74
Arne Limburg <arne@apache.org>
2013-01-03 22:25:29 +0000

    OWB-344: Implemented DefaultInterceptorHandler
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1428644 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-impl/src/main/java/org/apache/webbeans/intercept/DefaultInterceptorHandler.java
@@ -0,0 +1,64 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.webbeans.intercept;
+
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
+import javax.enterprise.inject.spi.InterceptionType;
+import javax.enterprise.inject.spi.Interceptor;
+
+import org.apache.webbeans.proxy.InterceptorHandler;
+
+public class DefaultInterceptorHandler<T> implements InterceptorHandler
+{
+
+    private T target;
+    private List<Method> methods;
+    private Map<Method, List<Interceptor<T>>> interceptors;
+    private Map<Interceptor<T>, T> instances;
+
+    public DefaultInterceptorHandler(T target,
+                                     List<Method> methods,
+                                     Map<Method, List<Interceptor<T>>> interceptors,
+                                     Map<Interceptor<T>, T> instances)
+    {
+        this.target = target;
+        this.methods = methods;
+        this.instances = instances;
+        this.interceptors = interceptors;
+    }
+
+    public Object invoke(int index, Object[] parameters)
+    {
+        try
+        {
+            Method method = methods.get(index);
+            List<Interceptor<T>> interceptors = this.interceptors.get(method);
+            InterceptorInvocationContext<T> ctx
+                = new InterceptorInvocationContext<T>(target, InterceptionType.AROUND_INVOKE, interceptors, instances, method, parameters);
+            return ctx.proceed();
+        }
+        catch (Exception e)
+        {
+            throw new IllegalStateException(e);
+        }
+    }
+}
