c8fc527f8196eefeafe9757daf7f587132338808
Thomas Andraschko <tandraschko@apache.org>
2013-04-05 12:25:20 +0000

    #OWB-802
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/branches/owb_1.1.x@1464948 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/main/java/org/apache/webbeans/annotation/AbstractAnnotationLiteral.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/annotation/AbstractAnnotationLiteral.java
@@ -20,9 +20,28 @@ package org.apache.webbeans.annotation;
 
 import javax.enterprise.util.AnnotationLiteral;
 import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 
 public class AbstractAnnotationLiteral<T extends Annotation> extends AnnotationLiteral<T>
 {
+    private Class<T> annotationType;
+
+    protected AbstractAnnotationLiteral()
+    {
+        this.annotationType = getAnnotationType(getClass());
+    }
+
+    /**
+     * Implemented for compatibility reasons with other cdi-api jar's.
+     * See OWB-802.
+     */
+    @Override
+    public Class<? extends Annotation> annotationType()
+    {
+        return annotationType;
+    }
+
     @Override
     public int hashCode()
     {
@@ -37,5 +56,46 @@ public class AbstractAnnotationLiteral<T extends Annotation> extends AnnotationL
         // implemented for performance reasons
         return Annotation.class.isInstance(other) &&
                 Annotation.class.cast(other).annotationType().equals(annotationType());
+    }
+
+    private Class<T> getAnnotationType(Class<?> definedClazz)
+    {
+        Type superClazz = definedClazz.getGenericSuperclass();
+
+        Class<T> clazz = null;
+
+        if (superClazz.equals(Object.class))
+        {
+            throw new RuntimeException("Super class must be parametrized type!");
+        }
+        else if (superClazz instanceof ParameterizedType)
+        {
+            ParameterizedType paramType = (ParameterizedType) superClazz;
+            Type[] actualArgs = paramType.getActualTypeArguments();
+
+            if (actualArgs.length == 1)
+            {
+                //Actual annotation type
+                Type type = actualArgs[0];
+
+                if (type instanceof Class)
+                {
+                    clazz = (Class<T>) type;
+                    return clazz;
+                }
+                else
+                {
+                    throw new RuntimeException("Not class type!");
+                }
+            }
+            else
+            {
+                throw new RuntimeException("More than one parametric type!");
+            }
+        }
+        else
+        {
+            return getAnnotationType((Class<?>) superClazz);
+        }
     }
 }
