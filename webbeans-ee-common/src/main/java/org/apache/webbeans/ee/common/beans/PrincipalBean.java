apache/openwebbeans

commit 5c96ce2875a6a5e2a43f8c6b0941aeec60e2cf2d

OWB-850 @Dependent on a produce -> no proxy should be created
git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1477205 13f79535-47bb-0310-9956-ffa450edef68

webbeans-impl/src/main/java/org/apache/webbeans/component/ResourceBean.java
@@ -24,6 +24,8 @@ import org.apache.webbeans.portable.ProviderBasedProxyProducerFactory;
 import org.apache.webbeans.spi.ResourceInjectionService;
 import org.apache.webbeans.spi.api.ResourceReference;
 
+import javax.enterprise.context.Dependent;
+
 public class ResourceBean<X, T extends Annotation> extends ProducerFieldBean<X>
 {
     
@@ -37,7 +39,8 @@ public class ResourceBean<X, T extends Annotation> extends ProducerFieldBean<X>
         super(ownerComponent,
               beanAttributes,
               beanClass,
-              new ProviderBasedProxyProducerFactory<X>(new ResourceProvider<X>(resourceReference, ownerComponent.getWebBeansContext()),
+              new ProviderBasedProxyProducerFactory<X>(Dependent.class.equals(beanAttributes.getScope()),
+                      new ResourceProvider<X>(resourceReference, ownerComponent.getWebBeansContext()),
                                                        beanClass,
                                                        ownerComponent.getWebBeansContext()));
         this.resourceReference = resourceReference;

webbeans-impl/src/main/java/org/apache/webbeans/component/ResourceProvider.java
@@ -18,13 +18,12 @@
  */
 package org.apache.webbeans.component;
 
-import javax.inject.Provider;
-
 import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.exception.WebBeansException;
 import org.apache.webbeans.spi.ResourceInjectionService;
 import org.apache.webbeans.spi.api.ResourceReference;
 
+import javax.inject.Provider;
 import java.io.ObjectStreamException;
 import java.io.Serializable;
 

webbeans-impl/src/main/java/org/apache/webbeans/portable/ProviderBasedProxyProducer.java
@@ -32,12 +32,14 @@ public class ProviderBasedProxyProducer<T> extends AbstractProducer<T>
     private Class<T> returnType;
     private Provider<T> provider;
     private T proxyInstance;
+    private boolean proxy;
 
-    public ProviderBasedProxyProducer(WebBeansContext webBeansContext, Class<T> returnType, Provider<T> provider)
+    public ProviderBasedProxyProducer(WebBeansContext webBeansContext, Class<T> returnType, Provider<T> provider, boolean proxy)
     {
         this.webBeansContext = webBeansContext;
         this.returnType = returnType;
         this.provider = provider;
+        this.proxy = proxy;
     }
 
     @Override
@@ -45,14 +47,21 @@ public class ProviderBasedProxyProducer<T> extends AbstractProducer<T>
     {
         if (proxyInstance == null)
         {
-            NormalScopeProxyFactory proxyFactory = webBeansContext.getNormalScopeProxyFactory();
-            ClassLoader loader = returnType.getClassLoader();
-            if (loader == null)
+            if (proxy)
             {
-                loader = WebBeansUtil.getCurrentClassLoader();
+                NormalScopeProxyFactory proxyFactory = webBeansContext.getNormalScopeProxyFactory();
+                ClassLoader loader = returnType.getClassLoader();
+                if (loader == null)
+                {
+                    loader = WebBeansUtil.getCurrentClassLoader();
+                }
+                Class<T> proxyClass = proxyFactory.createProxyClass(loader, returnType);
+                proxyInstance = proxyFactory.createProxyInstance(proxyClass, provider);
             }
-            Class<T> proxyClass = proxyFactory.createProxyClass(loader, returnType);
-            proxyInstance = proxyFactory.createProxyInstance(proxyClass, provider);
+            else
+            {
+                proxyInstance = provider.get();
+            }
         }
         return proxyInstance;
     }

webbeans-impl/src/main/java/org/apache/webbeans/portable/ProviderBasedProxyProducerFactory.java
@@ -32,8 +32,9 @@ public class ProviderBasedProxyProducerFactory<P> implements ProducerFactory<P>
     private Provider<?> provider;
     private Class<?> providerType;
     private WebBeansContext webBeansContext;
+    private boolean proxy;
     
-    public ProviderBasedProxyProducerFactory(Provider<?> provider, Class<?> providerType, WebBeansContext context)
+    public ProviderBasedProxyProducerFactory(boolean proxy, Provider<?> provider, Class<?> providerType, WebBeansContext context)
     {
         Asserts.assertNotNull(provider);
         Asserts.assertNotNull(providerType);
@@ -41,11 +42,12 @@ public class ProviderBasedProxyProducerFactory<P> implements ProducerFactory<P>
         this.provider = provider;
         this.providerType = providerType;
         this.webBeansContext = context;
+        this.proxy = proxy;
     }
 
     @Override
     public <T> Producer<T> createProducer(Bean<T> bean)
     {
-        return new ProviderBasedProxyProducer<T>(webBeansContext, (Class<T>)providerType, (Provider<T>)provider);
+        return new ProviderBasedProxyProducer<T>(webBeansContext, (Class<T>)providerType, (Provider<T>)provider, proxy);
     }
 }

commit 42aa41d72fb34c3f77aa364234b6357317e75954

OWB-850 fixed compile issues (broken since r1477205) and adjusted class names
git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1477318 13f79535-47bb-0310-9956-ffa450edef68

webbeans-ee-common/src/main/java/org/apache/webbeans/ee/common/beans/PrincipalBean.java
@@ -24,8 +24,9 @@ import org.apache.webbeans.component.BuildInOwbBean;
 import org.apache.webbeans.component.SimpleProducerFactory;
 import org.apache.webbeans.component.WebBeansType;
 import org.apache.webbeans.config.WebBeansContext;
-import org.apache.webbeans.portable.ProviderBasedProxyProducer;
+import org.apache.webbeans.portable.ProviderBasedProducer;
 
+//used by BeansDeployer (created via reflection)
 public class PrincipalBean extends BuildInOwbBean<Principal>
 {
 
@@ -34,7 +35,7 @@ public class PrincipalBean extends BuildInOwbBean<Principal>
         super(webBeansContext,
               WebBeansType.PRINCIPAL,
               Principal.class,
-              new SimpleProducerFactory<Principal>(new ProviderBasedProxyProducer<Principal>(webBeansContext, Principal.class, new PrincipalProvider(webBeansContext))));
+              new SimpleProducerFactory<Principal>(new ProviderBasedProducer<Principal>(webBeansContext, Principal.class, new PrincipalProvider(webBeansContext), true)));
     }
 
     @Override

webbeans-ee/src/main/java/org/apache/webbeans/ee/beans/UserTransactionBean.java
@@ -24,8 +24,9 @@ import org.apache.webbeans.component.BuildInOwbBean;
 import org.apache.webbeans.component.SimpleProducerFactory;
 import org.apache.webbeans.component.WebBeansType;
 import org.apache.webbeans.config.WebBeansContext;
-import org.apache.webbeans.portable.ProviderBasedProxyProducer;
+import org.apache.webbeans.portable.ProviderBasedProducer;
 
+//used by BeansDeployer (created via reflection)
 public class UserTransactionBean extends BuildInOwbBean<UserTransaction>
 {
 
@@ -35,7 +36,7 @@ public class UserTransactionBean extends BuildInOwbBean<UserTransaction>
               WebBeansType.USERTRANSACTION,
               UserTransaction.class,
               new SimpleProducerFactory<UserTransaction>(
-                      new ProviderBasedProxyProducer<UserTransaction>(webBeansContext, UserTransaction.class, new UserTransactionProvider(webBeansContext))));
+                      new ProviderBasedProducer<UserTransaction>(webBeansContext, UserTransaction.class, new UserTransactionProvider(webBeansContext), true)));
     }
 
     @Override

webbeans-ee/src/main/java/org/apache/webbeans/ee/beans/ValidatorBean.java
@@ -24,8 +24,9 @@ import org.apache.webbeans.component.BuildInOwbBean;
 import org.apache.webbeans.component.SimpleProducerFactory;
 import org.apache.webbeans.component.WebBeansType;
 import org.apache.webbeans.config.WebBeansContext;
-import org.apache.webbeans.portable.ProviderBasedProxyProducer;
+import org.apache.webbeans.portable.ProviderBasedProducer;
 
+//used by BeansDeployer (created via reflection)
 public class ValidatorBean extends BuildInOwbBean<Validator>
 {
 
@@ -34,7 +35,7 @@ public class ValidatorBean extends BuildInOwbBean<Validator>
         super(webBeansContext,
               WebBeansType.VALIDATION,
               Validator.class,
-              new SimpleProducerFactory<Validator>(new ProviderBasedProxyProducer<Validator>(webBeansContext, Validator.class, new ValidatorProvider(webBeansContext))));
+              new SimpleProducerFactory<Validator>(new ProviderBasedProducer<Validator>(webBeansContext, Validator.class, new ValidatorProvider(webBeansContext), true)));
     }
 
     @Override

webbeans-ee/src/main/java/org/apache/webbeans/ee/beans/ValidatorFactoryBean.java
@@ -24,8 +24,9 @@ import org.apache.webbeans.component.BuildInOwbBean;
 import org.apache.webbeans.component.SimpleProducerFactory;
 import org.apache.webbeans.component.WebBeansType;
 import org.apache.webbeans.config.WebBeansContext;
-import org.apache.webbeans.portable.ProviderBasedProxyProducer;
+import org.apache.webbeans.portable.ProviderBasedProducer;
 
+//used by BeansDeployer (created via reflection)
 public class ValidatorFactoryBean extends BuildInOwbBean<ValidatorFactory>
 {
 
@@ -35,7 +36,7 @@ public class ValidatorFactoryBean extends BuildInOwbBean<ValidatorFactory>
               WebBeansType.VALIDATIONFACT,
               ValidatorFactory.class,
               new SimpleProducerFactory<ValidatorFactory>(
-                      new ProviderBasedProxyProducer<ValidatorFactory>(webBeansContext, ValidatorFactory.class, new ValidatorFactoryProvider(webBeansContext))));
+                      new ProviderBasedProducer<ValidatorFactory>(webBeansContext, ValidatorFactory.class, new ValidatorFactoryProvider(webBeansContext), true)));
     }
     
     @Override

webbeans-impl/src/main/java/org/apache/webbeans/component/ResourceBean.java
@@ -20,7 +20,7 @@ package org.apache.webbeans.component;
 
 import java.lang.annotation.Annotation;
 
-import org.apache.webbeans.portable.ProviderBasedProxyProducerFactory;
+import org.apache.webbeans.portable.ProviderBasedProducerFactory;
 import org.apache.webbeans.spi.ResourceInjectionService;
 import org.apache.webbeans.spi.api.ResourceReference;
 
@@ -39,7 +39,7 @@ public class ResourceBean<X, T extends Annotation> extends ProducerFieldBean<X>
         super(ownerComponent,
               beanAttributes,
               beanClass,
-              new ProviderBasedProxyProducerFactory<X>(Dependent.class.equals(beanAttributes.getScope()),
+              new ProviderBasedProducerFactory<X>(Dependent.class.equals(beanAttributes.getScope()),
                       new ResourceProvider<X>(resourceReference, ownerComponent.getWebBeansContext()),
                                                        beanClass,
                                                        ownerComponent.getWebBeansContext()));

webbeans-impl/src/main/java/org/apache/webbeans/portable/ProviderBasedProducer.java
@@ -25,7 +25,7 @@ import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.proxy.NormalScopeProxyFactory;
 import org.apache.webbeans.util.WebBeansUtil;
 
-public class ProviderBasedProxyProducer<T> extends AbstractProducer<T>
+public class ProviderBasedProducer<T> extends AbstractProducer<T>
 {
 
     private WebBeansContext webBeansContext;
@@ -34,7 +34,7 @@ public class ProviderBasedProxyProducer<T> extends AbstractProducer<T>
     private T proxyInstance;
     private boolean proxy;
 
-    public ProviderBasedProxyProducer(WebBeansContext webBeansContext, Class<T> returnType, Provider<T> provider, boolean proxy)
+    public ProviderBasedProducer(WebBeansContext webBeansContext, Class<T> returnType, Provider<T> provider, boolean proxy)
     {
         this.webBeansContext = webBeansContext;
         this.returnType = returnType;

webbeans-impl/src/main/java/org/apache/webbeans/portable/ProviderBasedProducerFactory.java
@@ -26,7 +26,7 @@ import org.apache.webbeans.component.spi.ProducerFactory;
 import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.util.Asserts;
 
-public class ProviderBasedProxyProducerFactory<P> implements ProducerFactory<P>
+public class ProviderBasedProducerFactory<P> implements ProducerFactory<P>
 {
 
     private Provider<?> provider;
@@ -34,7 +34,7 @@ public class ProviderBasedProxyProducerFactory<P> implements ProducerFactory<P>
     private WebBeansContext webBeansContext;
     private boolean proxy;
     
-    public ProviderBasedProxyProducerFactory(boolean proxy, Provider<?> provider, Class<?> providerType, WebBeansContext context)
+    public ProviderBasedProducerFactory(boolean proxy, Provider<?> provider, Class<?> providerType, WebBeansContext context)
     {
         Asserts.assertNotNull(provider);
         Asserts.assertNotNull(providerType);
@@ -48,6 +48,6 @@ public class ProviderBasedProxyProducerFactory<P> implements ProducerFactory<P>
     @Override
     public <T> Producer<T> createProducer(Bean<T> bean)
     {
-        return new ProviderBasedProxyProducer<T>(webBeansContext, (Class<T>)providerType, (Provider<T>)provider, proxy);
+        return new ProviderBasedProducer<T>(webBeansContext, (Class<T>)providerType, (Provider<T>)provider, proxy);
     }
 }