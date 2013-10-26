ddaca24b8faa16b23d46689a72369965891b7d7f
Thomas Andraschko <tandraschko@apache.org>
2013-10-26 20:23:48 +0000

    #OWB-909
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1536034 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/main/java/org/apache/webbeans/component/ConversationBean.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/component/ConversationBean.java
@@ -24,6 +24,7 @@ import java.util.Collections;
 
 import javax.enterprise.context.Conversation;
 import javax.enterprise.context.RequestScoped;
+import javax.enterprise.inject.spi.PassivationCapable;
 
 import org.apache.webbeans.config.WebBeansContext;
 import org.apache.webbeans.conversation.ConversationImpl;
@@ -36,7 +37,7 @@ import org.apache.webbeans.util.CollectionUtil;
  * @version $Rev$ $Date$
  *
  */
-public class ConversationBean extends BuiltInOwbBean<ConversationImpl> implements InterceptedMarker
+public class ConversationBean extends BuiltInOwbBean<ConversationImpl> implements InterceptedMarker, PassivationCapable
 {
     /**
      * Default constructor.
