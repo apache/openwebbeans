f29be6f6981c8c702061ee7fbe2aa9950032ae6a
rsandtner <rsandtner@unknown>
2014-10-06 12:35:47 +0000

    OWB-992 added check for ParameterizedTypes
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1629632 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/test/decorators/tests/ExtendedGenericDecoratorTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/test/decorators/tests/ExtendedGenericDecoratorTest.java
@@ -22,6 +22,8 @@ import javax.annotation.Priority;
 import javax.decorator.Decorator;
 import javax.decorator.Delegate;
 import javax.enterprise.inject.Any;
+import javax.enterprise.inject.Decorated;
+import javax.enterprise.inject.spi.Bean;
 import javax.inject.Inject;
 
 import junit.framework.Assert;
@@ -35,36 +37,45 @@ import org.junit.Test;
 public class ExtendedGenericDecoratorTest extends AbstractUnitTest
 {
     @Test
-    @Ignore
     public void testExtendedGenericDecorator() throws Exception
     {
         addDecorator(ExtendedInterfaceDecorator.class);
 
-        startContainer(Interface.class, ExtendedInterface.class, LongService.class, IntegerService.class);
+        startContainer(Interface.class, ExtendedInterface.class, LongService.class, IntegerService.class, StringService.class);
 
         Assert.assertFalse(LongService.testMethodCalled);
         Assert.assertFalse(LongService.anotherMethodCalled);
         Assert.assertFalse(IntegerService.testMethodCalled);
         Assert.assertFalse(IntegerService.anotherMethodCalled);
-        Assert.assertFalse(ExtendedInterfaceDecorator.testMethodDecorated);
-        Assert.assertFalse(ExtendedInterfaceDecorator.anotherMethodDecorated);
+        Assert.assertFalse(ExtendedInterfaceDecorator.anotherMethodDecoratedWithLong);
+        Assert.assertFalse(ExtendedInterfaceDecorator.anotherMethodDecoratedWithInteger);
+        Assert.assertFalse(StringService.testMethodCalled);
 
+        // LongService
         ExtendedInterface<Long> service = getInstance(LongService.class);
+        service.anotherMethod(2L); // call method from interface - shall be decorated
+        service.test(1L); // call method from super interface - shall not be decorated
 
-        // call method from interface
-        service.anotherMethod(2L);
+        // StringService shall not be decorated
+        StringService stringService = getInstance(StringService.class);
+        stringService.test("test");
+
+        // IntegerService
+        IntegerService integerService = getInstance(IntegerService.class);
+        integerService.anotherMethod(3); // call method from interface - shall be decorated
+        integerService.test(4); // call method from super interface - shall not be decorated
+
 
         Assert.assertTrue(LongService.anotherMethodCalled);
-        Assert.assertTrue(ExtendedInterfaceDecorator.anotherMethodDecorated);
-
-        // call method from super interface
-        service.test(1L);
-
         Assert.assertTrue(LongService.testMethodCalled);
-        Assert.assertTrue(ExtendedInterfaceDecorator.testMethodDecorated);
+        Assert.assertTrue(ExtendedInterfaceDecorator.anotherMethodDecoratedWithLong);
+        Assert.assertTrue(IntegerService.anotherMethodCalled);
+        Assert.assertTrue(IntegerService.testMethodCalled);
+        Assert.assertTrue(ExtendedInterfaceDecorator.anotherMethodDecoratedWithInteger);
+        Assert.assertTrue(StringService.testMethodCalled);
+
 
         shutDownContainer();
-
     }
 
 
@@ -113,31 +124,54 @@ public class ExtendedGenericDecoratorTest extends AbstractUnitTest
         }
     }
 
+    public static class StringService implements Interface<String> {
+
+        public static boolean testMethodCalled = false;
+
+        @Override
+        public void test(String input)
+        {
+            testMethodCalled = true;
+        }
+    }
+
     @Decorator
     @Priority(value = 10)
-    public static class ExtendedInterfaceDecorator<T extends Number> implements ExtendedInterface<T> {
+    public abstract static class ExtendedInterfaceDecorator<T extends Number> implements ExtendedInterface<T> {
 
-        public static boolean testMethodDecorated = false;
-        public static boolean anotherMethodDecorated = false;
+        public static boolean anotherMethodDecoratedWithLong = false;
+        public static boolean anotherMethodDecoratedWithInteger = false;
+
+        public static boolean unexpectedDecorated = false;
 
         @Delegate
         @Inject
         @Any
         private ExtendedInterface<T> delegate;
 
+        @Inject
+        @Decorated
+        private Bean<Interface<T>> decorated;
+
 
         @Override
         public void anotherMethod(T input)
         {
-            anotherMethodDecorated = true;
-            delegate.anotherMethod(input);
-        }
+            Class<?> beanClass = decorated.getBeanClass();
+            if (beanClass.equals(LongService.class))
+            {
+                anotherMethodDecoratedWithLong = true;
+            }
+            else if (beanClass.equals(IntegerService.class))
+            {
+                anotherMethodDecoratedWithInteger = true;
+            }
+            else
+            {
+                unexpectedDecorated = true;
+            }
 
-        @Override
-        public void test(T input)
-        {
-            testMethodDecorated = true;
-            delegate.test(input);
+            delegate.anotherMethod(input);
         }
     }
 }
