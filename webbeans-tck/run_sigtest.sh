7b2b71e51f0d83a0d0c43ed0d54d2d15f88bfb8c
Mark Struberg <struberg@apache.org>
2015-03-24 21:48:57 +0000

    sigdevtest.jar is in ./lib of SIGTEST_HOME
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1669001 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-tck/run_sigtest.sh
+++ webbeans-tck/run_sigtest.sh
@@ -9,8 +9,8 @@
 
 mvn clean dependency:copy-dependencies
 
-java -jar ${SIGTEST_HOME}/sigtestdev.jar Setup -classpath ${JAVA_HOME}/jre/lib/rt.jar:./target/dependency/geronimo-jcdi_1.1_spec-1.0-alpha-1.jar:./target/dependency/geronimo-atinject_1.0_spec-1.0.jar:./target/dependency/geronimo-el_2.2_spec-1.0.2.jar:./target/dependency/geronimo-interceptor_1.2_spec-1.0-alpha-1.jar -Package javax.decorator -Package javax.enterprise -FileName cdi-api.sig -static
+java -jar ${SIGTEST_HOME}/lib/sigtestdev.jar Setup -classpath ${JAVA_HOME}/jre/lib/rt.jar:./target/dependency/geronimo-jcdi_1.1_spec-1.0-alpha-1.jar:./target/dependency/geronimo-atinject_1.0_spec-1.0.jar:./target/dependency/geronimo-el_2.2_spec-1.0.2.jar:./target/dependency/geronimo-interceptor_1.2_spec-1.0-alpha-1.jar -Package javax.decorator -Package javax.enterprise -FileName cdi-api.sig -static
 
 # now run the real sig test
-java -jar ${SIGTEST_HOME}/sigtestdev.jar SignatureTest -classpath ${JAVA_HOME}/jre/lib/rt.jar:./target/dependency/geronimo-jcdi_1.1_spec-1.0-alpha-1.jar:./target/dependency/geronimo-atinject_1.0_spec-1.0.jar:./target/dependency/geronimo-el_2.2_spec-1.0.2.jar:./target/dependency/geronimo-interceptor_1.2_spec-1.0-alpha-1.jar -Package javax.decorator -Package javax.enterprise -FileName cdi-api.sig -static
+java -jar ${SIGTEST_HOME}/lib/sigtestdev.jar SignatureTest -classpath ${JAVA_HOME}/jre/lib/rt.jar:./target/dependency/geronimo-jcdi_1.1_spec-1.0-alpha-1.jar:./target/dependency/geronimo-atinject_1.0_spec-1.0.jar:./target/dependency/geronimo-el_2.2_spec-1.0.2.jar:./target/dependency/geronimo-interceptor_1.2_spec-1.0-alpha-1.jar -Package javax.decorator -Package javax.enterprise -FileName cdi-api.sig -static
 
