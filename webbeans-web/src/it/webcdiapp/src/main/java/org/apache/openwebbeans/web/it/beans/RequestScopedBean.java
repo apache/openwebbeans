c891a7dafefc35af0b54b346ca40db9ca39eafa2
Reinhard Sandtner <rsandtner@apache.org>
2015-04-28 14:32:22 +0000

    OWB-1052 added it-folder and a simple test
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1676545 13f79535-47bb-0310-9956-ffa450edef68

---
+++ webbeans-web/src/it/webcdiapp/src/main/java/org/apache/openwebbeans/web/it/beans/RequestScopedBean.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.openwebbeans.web.it.beans;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Initialized;
+import javax.enterprise.context.RequestScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Named;
+
+@RequestScoped
+@Named
+public class RequestScopedBean
+{
+
+    private static int requestInstanceCounter = 0;
+    private static int requestContextCounter = 0;
+
+    private String name = "Super name";
+
+
+    @PostConstruct
+    protected void init()
+    {
+        requestInstanceCounter++;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getRequestInstanceCount()
+    {
+        return requestInstanceCounter;
+    }
+
+    public int getRequestContextCount()
+    {
+        return requestContextCounter;
+    }
+
+    public static void resetCounter()
+    {
+        requestContextCounter = 0;
+        requestInstanceCounter = 0;
+    }
+
+    public void onRequestInit(@Observes @Initialized(RequestScoped.class) Object payload)
+    {
+        requestContextCounter++;
+    }
+
+    public static String info()
+    {
+        return String.valueOf(requestInstanceCounter) + ',' + String.valueOf(requestContextCounter);
+    }
+}
---
+++ webbeans-web/src/it/webcdiapp/src/test/java/org/apache/openwebbeans/web/it/RequestScopedIT.java
@@ -0,0 +1,81 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.openwebbeans.web.it;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class RequestScopedIT
+{
+    public static final String BASE_URL = "http://localhost:8089/webbeanswebCdiApp";
+
+    @Test
+    public void testRequestScoped() throws Exception
+    {
+        DefaultHttpClient client = new DefaultHttpClient();
+        HttpGet jspGet = new HttpGet(BASE_URL + "/index.jsp");
+
+        checkResponse(client.execute(jspGet));
+        jspGet.releaseConnection();
+
+        HttpGet checkGet = new HttpGet(BASE_URL + "/check");
+        HttpResponse response = client.execute(checkGet);
+        checkResponse(response);
+
+        HttpEntity httpEntity = response.getEntity();
+        Assert.assertNotNull(httpEntity);
+
+        InputStream content = null;
+        try
+        {
+            content = httpEntity.getContent();
+            Assert.assertNotNull(content);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(content));
+            String result = bufferedReader.readLine();
+            Assert.assertNotNull(result);
+            Assert.assertEquals("2,2", result);
+        }
+        finally {
+            if (content != null)
+            {
+                content.close();
+            }
+        }
+
+
+        checkGet.releaseConnection();
+    }
+
+    private void checkResponse(HttpResponse response)
+    {
+        Assert.assertNotNull(response);
+        Assert.assertNotNull(response.getStatusLine());
+        Assert.assertEquals(200, response.getStatusLine().getStatusCode());
+    }
+
+}
