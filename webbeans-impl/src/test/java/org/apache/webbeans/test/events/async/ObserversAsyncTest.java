0c473efee0c27837cf012b5660176c65fbf4d2d2
Mark Struberg <struberg@apache.org>
2017-06-17 09:06:35 +0000

    OWB-1188 improve test to show problems with async error handling
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/trunk@1798998 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/test/java/org/apache/webbeans/test/events/async/ObserversAsyncTest.java
+++ webbeans-impl/src/test/java/org/apache/webbeans/test/events/async/ObserversAsyncTest.java
@@ -24,14 +24,18 @@ import javax.enterprise.inject.spi.Extension;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.IntStream;
 
 import org.apache.webbeans.test.AbstractUnitTest;
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -39,33 +43,49 @@ import static org.junit.Assert.assertNotNull;
 
 public class ObserversAsyncTest extends AbstractUnitTest
 {
+
+
     @Test
-    public void testAsyncEventExceptionHandling() throws ExecutionException, InterruptedException
+    public void testAsyncEventExceptionHandling_handle() throws ExecutionException, InterruptedException
     {
-        final int count = 100 + ForkJoinPool.getCommonPoolParallelism() * 10;
+        final int count = 10 + ForkJoinPool.getCommonPoolParallelism() * 5;
 
         final VisitorCollectorEvent event = new VisitorCollectorEvent();
 
-        addExtension(new Extension() {
-            void addABunchOfObserversAtLeastMoreThanThreads(@Observes final AfterBeanDiscovery afterBeanDiscovery)
-            {
-                IntStream.range(0, count)
-                        .forEach(i -> afterBeanDiscovery.addObserverMethod()
-                                .observedType(VisitorCollectorEvent.class)
-                                .async(true)
-                                .notifyWith(e ->
-                                {
-                                    if (i % 2 == 0 && (i < 30 || i > 70))
-                                    {
-                                        sleep(500);
-                                    }
+        addExtension(new ParallelObserveExtension(count));
+        startContainer();
 
-                                    final String name = "Observer" + i;
-                                    event.visiting(name);
-                                    throw new IllegalStateException(name);
-                                }));
-            }
-        });
+        BlockingQueue<Throwable> queue = new LinkedBlockingQueue<>();
+
+        long start = System.nanoTime();
+
+        getBeanManager().getEvent().fireAsync(event)
+            .handle((e, t) ->
+            {
+                return queue.offer(t);
+            });
+
+        Throwable t = queue.poll(20, TimeUnit.SECONDS);
+
+        long end = System.nanoTime();
+        long durationMs = TimeUnit.NANOSECONDS.toMillis(end - start);
+        System.out.println("took ms: " + durationMs);
+
+        Assert.assertNotNull(t);
+        Assert.assertTrue(t instanceof CompletionException);
+        CompletionException ce = (CompletionException) t;
+        Assert.assertEquals(count, ce.getSuppressed().length);
+
+    }
+
+    @Test
+    public void testAsyncEventExceptionHandling_CompletableFuture() throws ExecutionException, InterruptedException
+    {
+        final int count = 10 + ForkJoinPool.getCommonPoolParallelism() * 5;
+
+        final VisitorCollectorEvent event = new VisitorCollectorEvent();
+
+        addExtension(new ParallelObserveExtension(count));
         startContainer();
 
         final AtomicReference<Throwable> observerException = new AtomicReference<>();
@@ -116,6 +136,35 @@ public class ObserversAsyncTest extends AbstractUnitTest
         catch (InterruptedException e)
         {
             // ignore
+        }
+    }
+
+    private class ParallelObserveExtension implements Extension
+    {
+        private final int count;
+
+        public ParallelObserveExtension(int count)
+        {
+            this.count = count;
+        }
+
+        void addABunchOfObserversAtLeastMoreThanThreads(@Observes final AfterBeanDiscovery afterBeanDiscovery)
+        {
+            IntStream.range(0, count)
+                .forEach(i -> afterBeanDiscovery.<VisitorCollectorEvent>addObserverMethod()
+                    .observedType(VisitorCollectorEvent.class)
+                    .async(true)
+                    .notifyWith(e ->
+                    {
+                        if (i % 2 == 0 && (i < 30 || i > 70))
+                        {
+                            sleep(500);
+                        }
+
+                        final String name = "Observer" + i;
+                        e.getEvent().visiting(name);
+                        throw new IllegalStateException(name);
+                    }));
         }
     }
 }
