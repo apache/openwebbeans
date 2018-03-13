171ecc85331631360a602593af1bc661a75e1790
Romain Manni-Bucau <rmannibucau@apache.org>
2018-03-13 12:28:57 +0000

    OWB-1221 asm 6 upgrade - patch from jgallimore
    git-svn-id: https://svn.apache.org/repos/asf/openwebbeans/branches/owb_1.7.x@1826625 13f79535-47bb-0310-9956-ffa450edef68

--- webbeans-impl/src/main/java/org/apache/webbeans/proxy/InterceptorDecoratorProxyFactory.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/proxy/InterceptorDecoratorProxyFactory.java
@@ -25,11 +25,11 @@ import org.apache.webbeans.exception.WebBeansConfigurationException;
 import org.apache.webbeans.logger.WebBeansLoggerFacade;
 import org.apache.webbeans.util.Asserts;
 import org.apache.webbeans.util.ExceptionUtil;
-import org.apache.xbean.asm5.ClassWriter;
-import org.apache.xbean.asm5.Label;
-import org.apache.xbean.asm5.MethodVisitor;
-import org.apache.xbean.asm5.Opcodes;
-import org.apache.xbean.asm5.Type;
+import org.apache.xbean.asm6.ClassWriter;
+import org.apache.xbean.asm6.Label;
+import org.apache.xbean.asm6.MethodVisitor;
+import org.apache.xbean.asm6.Opcodes;
+import org.apache.xbean.asm6.Type;
 
 import javax.enterprise.inject.spi.Bean;
 import java.io.ObjectStreamException;
