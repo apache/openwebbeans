bf8a4a3dae6137851eccb32818e49d75bb84a022
vrossello <vrossello@travelcompositor.com>
2020-07-27 22:25:33 +0200

    Revert "remove redundant code"
    This reverts commit a38b13a89fbd3bcb929256a25b5dc38ff62aba1b.

--- webbeans-impl/src/main/java/org/apache/webbeans/config/BeansDeployer.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/config/BeansDeployer.java
@@ -1400,9 +1400,14 @@ public class BeansDeployer
             if (idx > 0)
             {
                 String previousPackage = name.substring(0, idx);
+                Boolean result = packageVetoCache.get(previousPackage);
+                if (result != null)
+                {
+                    return result;
+                }
                 while (true)
                 {
-                    Boolean result = packageVetoCache.get(previousPackage);
+                    result = packageVetoCache.get(previousPackage);
                     if (result != null)
                     {
                         return result;
