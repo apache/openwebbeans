f4e13057ea9c8d6f076535ada10beaf406a2fd2d
Romain Manni-Bucau <rmannibucau@gmail.com>
2020-12-16 15:02:50 +0100

    [OWB-1359] ensure default loader service is more easily extensible

--- webbeans-impl/src/main/java/org/apache/webbeans/service/DefaultLoaderService.java
+++ webbeans-impl/src/main/java/org/apache/webbeans/service/DefaultLoaderService.java
@@ -23,22 +23,20 @@ import org.apache.webbeans.logger.WebBeansLoggerFacade;
 import org.apache.webbeans.spi.LoaderService;
 import org.apache.webbeans.util.WebBeansUtil;
 
-import java.util.ArrayList;
+import javax.enterprise.inject.spi.Extension;
 import java.util.List;
 import java.util.ServiceLoader;
 import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+import static java.util.stream.Collectors.toList;
 
 /**
- * Default implementation which delegates to the s{@link ServiceLoader} of Java 1.6 and
- * uses a fallback for Java 1.5
+ * Default implementation which delegates to the s{@link ServiceLoader}.
  */
 public class DefaultLoaderService implements LoaderService
 {
-    private static final Logger logger = WebBeansLoggerFacade.getLogger(DefaultLoaderService.class);
-
-    private static final boolean JAVA_6_AVAILABLE = isJava6();
-
     @Override
     public <T> List<T> load(Class<T> serviceType)
     {
@@ -48,44 +46,30 @@ public class DefaultLoaderService implements LoaderService
     @Override
     public <T> List<T> load(Class<T> serviceType, ClassLoader classLoader)
     {
-        if(JAVA_6_AVAILABLE)
-        {
-            List<T> result = new ArrayList<>();
-            try
-            {
-                ServiceLoader<T> services;
-                services = ServiceLoader.load(serviceType, classLoader);
-
-                for (T service : services)
-                {
-                    result.add(service);
-                }
-            }
-            catch (Error error)
-            {
-                // WTF! ServiceLoader is cool, but THAT is utter crap: it throws some Errors!
-                logger.log(Level.SEVERE, "Problem while loading CDI Extensions", error);
-                throw new WebBeansConfigurationException("Problem while loading CDI Extensions", error);
-            }
-
-
-            return result;
-        }
-
-        return new ManualImplementationLoaderService<>(serviceType, classLoader).loadServiceImplementations();
-    }
-
-    private static boolean isJava6()
-    {
         try
         {
-            ServiceLoader.class.getName();
-            return true;
+            Stream<T> stream = StreamSupport.stream(ServiceLoader.load(serviceType, classLoader).spliterator(), false);
+            if (Extension.class == serviceType)
+            {
+                return mapExtensions(stream).collect(toList());
+            }
+            // OWBPlugin
+            return stream.collect(toList());
         }
-        catch (NoClassDefFoundError error)
+        catch (Error error)
         {
-            logger.info("Using Java 5 compatibility mode, because didn't find ServiceLoader: " + error);
-            return false;
+            // WTF! ServiceLoader is cool, but THAT is utter crap: it throws some Errors!
+            WebBeansLoggerFacade.getLogger(DefaultLoaderService.class)
+                    .log(Level.SEVERE, "Problem while loading CDI Extensions", error);
+            throw new WebBeansConfigurationException("Problem while loading CDI Extensions", error);
         }
+    }
+
+    // enables to easily extend the loader to customize extensions:
+    // 1. filter some undesired extensions programmatically
+    // 2. remap some extensions (to drop some events or wrap them for ex)
+    protected  <T> Stream<T> mapExtensions(final Stream<T> stream)
+    {
+        return stream;
     }
 }
